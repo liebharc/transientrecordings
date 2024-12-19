28fbe0164deb56a3d08d06605a2c03c8c52de46a
Christian Liebhardt <christian.liebhardt@arcor.de>
2024-12-19 23:49:18 +0100

    Updated README

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -182,7 +182,7 @@ export default function Home() {
 
   return (
     <div className="flex justify-center min-h-screen">
-      <main className="flex flex-col gap-8 pt-[33vh]">
+      <main className="flex flex-col gap-8 pt-[33vh] px-2">
         <div className="flex gap-4 justify-center">
           <Button
             onClick={handleRecord}
